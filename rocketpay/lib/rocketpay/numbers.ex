7fae2fa0bab7bbb77ca6548d19d374e6744faa5a
João Giovanni <joaogiovannivilela@gmail.com>
2021-02-22 21:50:09 -0300

    Using PipeOperator, String.split and Enum.map

--- rocketpay/lib/rocketpay/numbers.ex
+++ rocketpay/lib/rocketpay/numbers.ex
@@ -1,10 +1,23 @@
 defmodule Rocketpay.Numbers do
   def sum_from_file(filename) do
-    file = File.read("#{filename}.csv")
-    handle_file(file)
+    # Pipe Operator
+    "#{filename}.csv"
+    |> File.read()
+    |> handle_file()
   end
 
   # Pattern Matching
-  defp handle_file({:ok, file}), do: file
+  defp handle_file({:ok, result}) do
+    result
+    |> String.split(",")
+    |> Enum.map(fn number -> String.to_integer(number) end)
+    |> Enum.sum()
+
+    # or
+    # result = String.split(result, ",")
+    # result = Enum.map(result, fn number -> String.to_integer(number) end)
+    # result = Enum.sum(result)
+    # result ## in Elixir, the last line is already the return
+  end
   defp handle_file({:error, _reason}), do: {:error, "Invalid file!"}
 end
