3490224b0ebc77a1c9a9b010bfe040ee446f0249
João Giovanni <joaogiovannivilela@gmail.com>
2021-02-24 21:20:21 -0300

    Using a FallbackController

--- rocketpay/lib/rocketpay_web/controllers/users_controller.ex
+++ rocketpay/lib/rocketpay_web/controllers/users_controller.ex
@@ -1,24 +1,16 @@
 defmodule RocketpayWeb.UsersController do
   use RocketpayWeb, :controller
 
-  alias Rocketpay
+  import Rocketpay, only: [create_user: 1]
+  alias Rocketpay.User
+
+  action_fallback RocketpayWeb.FallbackController
 
   def create(conn, params) do
-    params
-    |> Rocketpay.create_user()
-    |> handle_response(conn)
-  end
-
-  defp handle_response({:ok, %Rocketpay.User{} = user}, conn) do
-    conn
-    |> put_status(:created)
-    |> render("create.json", user: user)
-  end
-
-  defp handle_response({:error, result}, conn) do
-    conn
-    |> put_status(:bad_request)
-    |> put_view(RocketpayWeb.ErrorView)
-    |> render("400.json", result: result)
+    with {:ok, %User{} = user} <- create_user(params) do
+      conn
+      |> put_status(:created)
+      |> render("create.json", user: user)
+    end
   end
 end
