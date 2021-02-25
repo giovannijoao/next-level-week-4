2968b8a9f1a599b639410ed6ecde2f6163c5941b
João Giovanni <joaogiovannivilela@gmail.com>
2021-02-25 18:42:24 -0300

    Transactions between accounts

---
+++ rocketpay/lib/rocketpay/accounts/transaction.ex
@@ -0,0 +1,27 @@
+defmodule Rocketpay.Accounts.Transaction do
+  alias Ecto.Multi
+  alias Rocketpay.Repo
+  alias Rocketpay.Accounts.Operation
+
+  def call(%{
+    "from" => from_id,
+    "to" => to_id,
+    "value" => value
+  }) do
+    widthdraw_params = build_params(from_id, value);
+    deposit_params = build_params(to_id, value);
+    Multi.new()
+    |> Multi.merge(fn _changes -> Operation.call(widthdraw_params, :withdraw) end)
+    |> Multi.merge(fn _changes -> Operation.call(deposit_params, :deposit) end)
+    |> run_transaction()
+  end
+
+  defp build_params(id, value), do: %{"id" => id, "value" => value}
+
+  defp run_transaction(multi) do
+    case Repo.transaction(multi) do
+      {:error, _operation, reason, _changes} -> {:error, reason}
+      {:ok, %{deposit: account}} -> {:ok, account}
+    end
+  end
+end
